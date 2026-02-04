e61991f630527eaf21e4aa24778db3b7b7532bc5
Steven Lewi <steven@stevenlewi.id>
2026-02-04 02:55:50 +0000

    [LeetCode Sync] - Valid Parentheses - Runtime - 4 ms (93.19%), Memory - 41.2 MB (89.74%)

---
+++ src/main/kotlin/p0020_valid_parentheses/solution.kt
@@ -0,0 +1,25 @@
+package p0020_valid_parentheses
+
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = Stack<Char>()
+        for (c in s) {
+            if (stack.size == 0) {
+                stack.push(c)
+            } else if (c == ')') {
+                if (stack.peek() != '(') return false
+                else stack.pop()
+            } else if (c == ']') {
+                if (stack.peek() != '[') return false
+                else stack.pop()
+            } else if (c == '}') {
+                if (stack.peek() != '{') return false
+                else stack.pop()
+            } else {
+                stack.push(c)
+            }
+        }
+
+        return stack.size == 0
+    }
+}
