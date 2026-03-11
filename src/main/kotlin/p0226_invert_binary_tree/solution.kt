9b679cd0ec4cb4d0276cf5f73884a3e0034583d8
Steven Lewi <steven@stevenlewi.id>
2026-03-11 07:48:57 +0000

    [LeetCode Sync] - Invert Binary Tree - Runtime - 0 ms (100.00%), Memory - 41.3 MB (13.77%)

---
+++ src/main/kotlin/p0226_invert_binary_tree/solution.kt
@@ -0,0 +1,25 @@
+package p0226_invert_binary_tree
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root?.left != null || root?.right != null) {
+            var temp = root?.left
+            root?.left = root?.right
+            root?.right = temp
+            invertTree(root?.left)
+            invertTree(root?.right)
+        }
+
+        return root
+    }
+}
