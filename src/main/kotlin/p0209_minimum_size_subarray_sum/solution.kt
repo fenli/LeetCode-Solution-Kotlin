5cffcfcbe1d12506d719ea4a6f36071c31c03ffb
Steven Lewi <steven@stevenlewi.id>
2026-03-12 07:10:40 +0000

    [LeetCode Sync] - Minimum Size Subarray Sum - Runtime - 1 ms (100.00%), Memory - 61.6 MB (80.41%)

---
+++ src/main/kotlin/p0209_minimum_size_subarray_sum/solution.kt
@@ -0,0 +1,29 @@
+package p0209_minimum_size_subarray_sum
+
+// [2,3,1,2,4,3]
+// 2 3 1 2
+//   3 1 2 4
+//     1 2 4
+//       2 4 3
+//         4 3
+//           3
+class Solution {
+    fun minSubArrayLen(target: Int, nums: IntArray): Int {
+        var minLen = Int.MAX_VALUE
+        var sums = 0
+        var subIndex = 0
+        for (i in nums.indices) {
+            sums += nums[i]
+            if (sums >= target) {
+
+                do { 
+                    sums = sums - nums[subIndex]
+                    minLen = min(minLen, i - subIndex + 1)
+                    subIndex++
+                } while(sums >= target)
+            }
+        }
+
+        return if (minLen == Int.MAX_VALUE) 0 else minLen
+    }
+}
