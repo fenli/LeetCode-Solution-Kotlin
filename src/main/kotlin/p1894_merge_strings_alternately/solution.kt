423db1d9e0cb0a55a13bfe3d7c66ada8491e1a1b
Steven Lewi <steven@stevenlewi.id>
2026-03-16 11:57:08 +0000

    [LeetCode Sync] - Merge Strings Alternately - Runtime - 87 ms (90.42%), Memory - 40 MB (86.04%)

---
+++ src/main/kotlin/p1894_merge_strings_alternately/solution.kt
@@ -0,0 +1,12 @@
+package p1894_merge_strings_alternately
+
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        return buildString {
+            for (i in 0 until max(word1.length, word2.length)) {
+                if (i < word1.length) append(word1[i])
+                if (i < word2.length) append(word2[i])
+            }
+        }
+    }
+}
