0e6898cd347cd6d99d6c3dc655ca607a9f081467
Steven Lewi <steven@stevenlewi.id>
2026-03-27 14:30:00 +0700

    Solution #104

---
+++ src/main/kotlin/104. Maximum Depth of Binary Tree.kt
@@ -0,0 +1,32 @@
+import kotlin.math.max
+
+/**
+ * Problem : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ * Submission : https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/1960932805
+ */
+
+class Solution104 {
+    data class TreeNode(var `val`: Int, var left: TreeNode? = null, var right: TreeNode? = null)
+
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+        return 1 + max(maxDepth(root?.left), maxDepth(root?.right))
+    }
+}
+
+fun main() {
+    fun List<Int?>.toTreeNode(index: Int, depth: Int): Solution104.TreeNode? {
+        if (index >= size) return null
+        if (get(index) == null) return null
+
+        return Solution104.TreeNode(
+            get(index)!!,
+            toTreeNode(index * depth + 1, depth + 1),
+            toTreeNode(index * depth + 2, depth + 1)
+        )
+    }
+
+    val solution = Solution104()
+    check(solution.maxDepth(listOf(3, 9, 20, null, null, 15, 7).toTreeNode(0, 1)) == 3)
+    check(solution.maxDepth(listOf(1, null, 2).toTreeNode(0, 1)) == 2)
+}
