01dfe7c9bb1239f2bf9c1ea0e5c4dbcca1af27fd
Steven Lewi <steven@stevenlewi.id>
2026-03-30 11:03:00 +0000

    [LeetCode Sync] - Search in a Binary Search Tree - Runtime - 0 ms (100.00%), Memory - 47.4 MB (99.02%)

---
+++ src/main/kotlin/p0783_search_in_a_binary_search_tree/solution.kt
@@ -0,0 +1,20 @@
+package p0783_search_in_a_binary_search_tree
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        if (root == null) return null
+        else if (root?.`val` == `val`) return root
+        else if (root!!.`val` > `val`) return searchBST(root?.left, `val`)
+        else return searchBST(root?.right, `val`)
+    }
+}
