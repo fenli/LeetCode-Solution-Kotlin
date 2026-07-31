a45836a2a314746693ed19ea4d48608dff3b8ea9
Steven Lewi <steven@stevenlewi.id>
2026-07-31 18:29:20 +0000

    [LeetCode Sync] - Concatenation of Array - Runtime - 1 ms (97.73%), Memory - 46.1 MB (73.01%)

---
+++ src/main/kotlin/p2058_concatenation_of_array/solution.kt
@@ -0,0 +1,14 @@
+package p2058_concatenation_of_array
+
+class Solution {
+    fun getConcatenation(nums: IntArray): IntArray {
+        val n = nums.size
+        val ans = IntArray(n * 2)
+        for (i in 0 until n) {
+            ans[i] = nums[i]
+            ans[i + n] = nums[i]
+        }
+
+        return ans
+    }
+}
