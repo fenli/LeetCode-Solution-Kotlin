6277b8d54f083aadf2d07b4b169327af302af463
Steven Lewi <steven@stevenlewi.id>
2026-08-20 07:53:42 +0000

    [LeetCode Sync] - Distribute Elements Into Two Arrays I - Runtime - 10 ms (100.00%), Memory - 45.7 MB (100.00%)

---
+++ src/main/kotlin/p3347_distribute_elements_into_two_arrays_i/solution.kt
@@ -0,0 +1,16 @@
+package p3347_distribute_elements_into_two_arrays_i
+
+class Solution {
+    fun resultArray(nums: IntArray): IntArray {
+        val arr1 = mutableListOf<Int>()
+        val arr2 = mutableListOf<Int>()
+        arr1.add(nums[0])
+        arr2.add(nums[1])
+        for (i in 2 until nums.size) {
+            if (arr1.last() > arr2.last()) arr1.add(nums[i])
+            else arr2.add(nums[i])
+        }
+
+        return (arr1 + arr2).toIntArray()
+    }
+}
