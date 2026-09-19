fa6cdb9a0dd560305f8d4472576b4bd9c4e95971
Steven Lewi <steven@stevenlewi.id>
2026-09-19 04:01:31 +0000

    [LeetCode Sync] - Circle and Rectangle Overlapping - Runtime - 3 ms (0.00%), Memory - 40 MB (50.00%)

---
+++ src/main/kotlin/p1501_circle_and_rectangle_overlapping/solution.kt
@@ -0,0 +1,29 @@
+package p1501_circle_and_rectangle_overlapping
+
+class Solution {
+    fun checkOverlap(
+        radius: Int,
+        xCenter: Int,
+        yCenter: Int,
+        x1: Int,
+        y1: Int,
+        x2: Int,
+        y2: Int,
+    ): Boolean {
+        val x = closest(xCenter, x1..x2)
+        val y = closest(yCenter, y1..y2)
+        return square(x - xCenter) <= square(radius) - square(y - yCenter)
+    }
+
+    private fun closest(
+        p0: Int,
+        range: IntRange,
+    ): Int =
+        when {
+            p0 < range.first -> range.first
+            p0 > range.last -> range.last
+            else -> p0
+        }
+
+    private fun square(x: Int): Int = x * x
+}
